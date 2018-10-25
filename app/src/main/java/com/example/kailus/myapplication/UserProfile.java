c0c706e1d3edbf0bdfedeb5ff69925819d27fb55
mrcolemank24 <mrcolemank24@gmail.com>
2018-10-25 18:47:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kailus/myapplication/UserProfile.java
@@ -0,0 +1,13 @@
+package com.example.kailus.myapplication;
+
+public class UserProfile {
+    public String UserProfile;
+    public String userEmail;
+    public String userName;
+
+    public UserProfile(String userProfile, String userEmail, String userName) {
+        UserProfile = userProfile;
+        this.userEmail = userEmail;
+        this.userName = userName;
+    }
+}
